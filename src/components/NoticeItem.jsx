c050d66a0257c164062bab1aabe5bc4850fef660
wematu <simplyubiquitous2020@gmail.com>
2024-10-04 11:04:49 +0000

    style notice card

--- src/components/NoticeItem.jsx
+++ src/components/NoticeItem.jsx
@@ -1,9 +1,18 @@
 import { StyleSheet, TouchableOpacity } from "react-native";
 import React from "react";
-import { Card, Text } from "react-native-paper";
+import {
+  Button,
+  Card,
+  Divider,
+  IconButton,
+  Text,
+  useTheme,
+} from "react-native-paper";
 import { useNavigation } from "@react-navigation/native";
 
 const NoticeCard = ({ notice }) => {
+  const theme = useTheme();
+
   const navigation = useNavigation();
 
   return (
@@ -11,11 +20,39 @@ const NoticeCard = ({ notice }) => {
       style={styles.card}
       onPress={() => navigation.navigate("NoticeDetail", { id: notice._id })}
     >
-      <Card mode="elevated">
-        <Card.Title title={notice.title} />
-        <Card.Content>
-          <Text>{notice.content}</Text>
+      <Card mode="contained">
+        <Card.Title
+          right={(props) => (
+            <IconButton {...props} icon="new-box" onPress={() => {}} />
+          )}
+          title={<Text variant="titleLarge">{notice.title}</Text>}
+        />
+
+        <Divider style={{ marginTop: -15, marginBottom: 10 }} />
+
+        <Card.Content style={{}}>
+          <Text>
+            {notice.content.substring(0, 250)}
+            {notice.content.length > 250 && "..."}
+          </Text>
+          <Text style={{ color: theme.colors.backdrop, marginLeft: "auto" }}>
+            By {notice.createdBy} | {notice.createdAt}
+          </Text>
         </Card.Content>
+
+        <Divider />
+
+        <Card.Actions
+          style={{
+            justifyContent: "space-between",
+            flexDirection: "row",
+            marginVertical: -5,
+          }}
+        >
+          <Button compact labelStyle={{}} mode="text">
+            Read More
+          </Button>
+        </Card.Actions>
       </Card>
     </TouchableOpacity>
   );
