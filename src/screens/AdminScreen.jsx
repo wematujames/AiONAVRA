f8fce35e0711c47487cd2eae83d649f26fa4cbd8
wematu <simplyubiquitous2020@gmail.com>
2024-10-06 16:15:22 +0000

    attempt: fix drawer height

--- src/screens/AdminScreen.jsx
+++ src/screens/AdminScreen.jsx
@@ -7,68 +7,69 @@ import UsersListScreen from "./UsersListScreen";
 import { createDrawerNavigator } from "@react-navigation/drawer";
 import RouteListScreen from "./RouteListScreen";
 import NotificationsIcon from "../components/NoficationsIcon";
-import { SafeAreaView } from "react-native-safe-area-context";
 
 const Drawer = createDrawerNavigator();
 
 const AdminScreen = ({}) => {
+  // <SafeAreaView style={{ flex: 1 }}>
   return (
-    <SafeAreaView style={{ flex: 1 }}>
-      <Drawer.Navigator
-        initialRouteName="UsersListScreen"
-        screenOptions={{
-          drawerLabel: "Home",
-          headerShown: true,
-          drawerType: "front",
-          headerTintColor: "black",
-          headerRight: () => <NotificationsIcon />,
-          // drawerStyle: {
-          //   height: "90%", // Adjust this to limit the drawer height
-          // },
+    <Drawer.Navigator
+      initialRouteName="UsersListScreen"
+      screenOptions={{
+        drawerLabel: "Home",
+        headerShown: true,
+        drawerType: "front",
+        headerTintColor: "black",
+        headerRight: () => <NotificationsIcon />,
+        // drawerStyle: {
+        //   height: "90%", // Adjust this to limit the drawer height
+        // },
+      }}
+    >
+      <Drawer.Screen
+        name="Home"
+        component={HomeScreen}
+        options={{
+          title: "Home",
+          drawerIcon: () => <MaterialIcons size={20} name="home" />,
         }}
-      >
-        <Drawer.Screen
-          name="Home"
-          component={HomeScreen}
-          options={{
-            title: "Home",
-            drawerIcon: () => <MaterialIcons size={20} name="home" />,
-          }}
-        />
+      />
 
-        <Drawer.Screen
-          name="RouteList"
-          component={RouteListScreen}
-          options={{
-            headerShown: true,
-            title: "Routes",
-            drawerLabel: "Routes",
-            drawerIcon: () => <MaterialIcons size={20} name="alt-route" />,
-          }}
-        />
+      <Drawer.Screen
+        name="RouteList"
+        component={RouteListScreen}
+        options={{
+          headerShown: true,
+          title: "Routes",
+          drawerLabel: "Routes",
+          drawerIcon: () => <MaterialIcons size={20} name="alt-route" />,
+        }}
+      />
 
-        <Drawer.Screen
-          name="UsersListScreen"
-          component={UsersListScreen}
-          options={{
-            title: "Users",
-            drawerIcon: () => (
-              <MaterialIcons size={20} name="supervised-user-circle" />
-            ),
-          }}
-        />
+      <Drawer.Screen
+        name="UsersListScreen"
+        component={UsersListScreen}
+        options={{
+          title: "Users",
+          drawerIcon: () => (
+            <MaterialIcons size={20} name="supervised-user-circle" />
+          ),
+        }}
+      />
 
-        <Drawer.Screen
-          name="FeedbackList"
-          component={FeedbackListScreen}
-          options={{
-            title: "Feedback",
-            drawerIcon: () => <MaterialIcons size={20} name="feedback" />,
-          }}
-        />
-      </Drawer.Navigator>
-    </SafeAreaView>
+      <Drawer.Screen
+        name="FeedbackList"
+        component={FeedbackListScreen}
+        options={{
+          title: "Feedback",
+          drawerIcon: () => <MaterialIcons size={20} name="feedback" />,
+        }}
+      />
+    </Drawer.Navigator>
   );
+  {
+    /* </SafeAreaView> */
+  }
 };
 
 export default AdminScreen;
